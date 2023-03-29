3fb4c2780ceae9a312324fc26a06c10eb1c1ac28
Rafael José <ra.david@hotmail.com>
2023-03-29 12:47:38 +0100

    Exercise 4
    Add sleep time 500 milliseconds

--- Tempo/Program.cs
+++ Tempo/Program.cs
@@ -12,6 +12,12 @@ namespace Tempo
             Stopwatch crono1 = new Stopwatch();
             // This code creates a new Stopwatch object called crono2.
             Stopwatch crono2 = new Stopwatch();
+
+            // This code starts the Stopwatch object crono1.
+            crono1.Start();
+
+            // Add sleep time 500 milliseconds.
+            Thread.Sleep(500);
         }
     }
 }
