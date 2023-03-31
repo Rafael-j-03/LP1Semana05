4c2745a5b6b32c423a320c7af1bb4df1d27e797c
Rafael José <ra.david@hotmail.com>
2023-03-31 13:10:17 +0100

    Exercise 9
    Put the array of arguments into lower case
    
    Print all the arguments

--- LowerUnion/Program.cs
+++ LowerUnion/Program.cs
@@ -6,7 +6,17 @@ namespace LowerUnion
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Put all the array of args into lower case
+            for (int i = 0; i < args.Length; i++)
+            {
+                args[i] = args[i].ToLower();
+            }
+
+            // Print the array of args
+            for (int i = 0; i < args.Length; i++)
+            {
+                Console.WriteLine(args[i]);
+            }
         }
     }
 }
